9eb10b4950a03c306b0ea2bb0e35b4e451e9e60b
Enrique Crespo <jecrespom@gmail.com>
2017-07-03 00:54:57 +0200

    Ejercicio02

---
+++ Ejercicio02-LDR/Ejercicio02-LDR.ino
@@ -0,0 +1,20 @@
+#define PIN_LDR A1
+
+int maximo = 0;
+int minimo = 1023;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(PIN_LDR);
+  maximo = max(maximo,sensorValue);
+  minimo = max(minimo,sensorValue);
+  Serial.print(sensorValue);
+  Serial.print(",");
+  Serial.print(maximo);
+  Serial.print(",");
+  Serial.println(minimo);
+  delay(500);
+}
